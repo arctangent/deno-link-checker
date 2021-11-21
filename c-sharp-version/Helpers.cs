a110c039cb611d73204ef1751e2b6df02f8b29ec
Jacob Conrad Martin <arctangent@gmail.com>
2021-11-21 12:06:39 +0000

    Original C# crap for reference

---
+++ c-sharp-version/Helpers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text.Json;
+using System.Threading;
+
+namespace LinkChecker.ConsoleApp
+{
+    public static class Helpers
+    {
+
+        public static void RandomSleep()
+        {
+            Random rnd = new();
+            int sleepMilliseconds = rnd.Next(250, 500);
+            Thread.Sleep(sleepMilliseconds);
+        }
+
+        public static void DebugPrint(object obj)
+        {
+            string jsonString = JsonSerializer.Serialize(obj);
+            Console.WriteLine("\n\nDEBUG:\n\n{0}\n\n", jsonString);
+        }
+
+        public static HashSet<string> GetInternalHrefs(Uri baseUrl, string content)
+        {
+            HashSet<string> hrefs = RegexMatch(content);
+
+            // Remove obvious problems
+            hrefs.RemoveWhere(h => h.Length == 0);
+            hrefs.RemoveWhere(h => h.StartsWith('#'));
+
+            // Remove link to self
+            hrefs.RemoveWhere(h => baseUrl.ToString().EndsWith(h));
+            hrefs.RemoveWhere(h => baseUrl.ToString().EndsWith(h+'/'));
+
+            // TODO: Need a better way to exclude non-http(s) schemes
+            hrefs.RemoveWhere(h => h.StartsWith("tel:"));
+            hrefs.RemoveWhere(h => h.StartsWith("email:"));
+
+            // TODO: Need better way to exclude parts of a site
+            hrefs.RemoveWhere(h => h.Contains("service-search"));
+
+            string targetUrl = baseUrl.ToString();
+            HashSet<string> results = new();
+
+            foreach (string href in hrefs.ToList())
+            {
+                string newUrl = href;
+
+                // Handle URLs beginning with a slash 
+                if (newUrl.StartsWith('/'))
+                {
+                    newUrl = targetUrl + newUrl.TrimStart('/');
+                }
+
+                // Ignore anything external
+                if (!newUrl.StartsWith(targetUrl))
+                {
+                    continue;
+                }
+
+                // Remove querystring and anchor links
+                newUrl = newUrl.Split("?")[0];
+                newUrl = newUrl.Split("#")[0];
+
+                // Hopefully the URL is now OK
+                results.Add(newUrl);
+
+            }
+
+            return results;
+        }
+
+        private static HashSet<string> RegexMatch(string inputString)
+        {
+            HashSet<string> hrefs = new();
+
+            Match m;
+            string HRefPattern = @"href\s*=\s*(?:[""'](?<1>[^""']*)[""']|(?<1>[^>\s]+))";
+ 
+            m = Regex.Match(inputString, HRefPattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            while (m.Success)
+            {
+                hrefs.Add(m.Groups[1].ToString().ToLower());
+                m = m.NextMatch();
+            }
+
+            return hrefs;
+        }
+    }
+}
---
+++ c-sharp-version/Models.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkChecker.ConsoleApp
+{
+
+    public class LinkCheckerContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+
+        private readonly string _file;
+
+        public LinkCheckerContext(string rootUrl)
+        {
+            _file = new Uri(rootUrl).Host + ".db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=" + _file);
+        }
+
+    }
+
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Url { get; set; }
+        public int StatusCode { get; set; }
+        public string ContentType { get; set; }
+
+        // SQLite can't handle DateTimeOffset properly,
+        // so we will expect events scan/scrape events
+        // may trigger early/late when clocks change
+        public DateTime MetaLastScanned { get; set; }
+        public DateTime MetaLastScraped { get; set; }
+
+        // Hopefully this approach lets us store data quicker
+        // (although it may well take longer to pull back out...)
+        // TODO: Could I use a ValueConverter to transform
+        //       from string <--> List<string> to make code tidier?
+        public string InboundUrls { get; set; }
+        public string OutboundUrls { get; set; }
+    }
+
+}
