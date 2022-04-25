cec48cc46ea87aa7ea009d4bc909b8b7ba8cdbed
Jacob Conrad Martin <arctangent@gmail.com>
2022-04-25 11:33:04 +0100

    Update

--- report.ts
+++ report.ts
@@ -13,7 +13,7 @@ const db = new Database(`./data/${project}/db.json`);
 const data = db.db.documents;
 
 // Generate the responses report
-let redirects = "url,status,redirected,redirectsTo";
+let redirects = "url,status,redirected,location";
 for (const item of data) {
     redirects += `\n${item.url},${item.status},${item.redirected},`;
     if (item.url != item.location) redirects += item.location;
@@ -22,22 +22,31 @@ for (const item of data) {
 Deno.writeTextFileSync(`./data/${project}/redirects.csv`, redirects);
 
 
-// Generate the inbound links report
-let inbounds = "url,status,inbounds";
+// Build map of inbound links
+
 const map = new Map<string, string[]>();
 for (const item of data) {
     for (const hyperlink of item.hyperlinks) {
         const inbounds = map.get(hyperlink);
         if (!inbounds) {
-            map.set(hyperlink, [item.location]);
+            map.set(hyperlink, [item.url]);
         } else {
-            inbounds.push(item.location);
+            inbounds.push(item.url);
             map.set(hyperlink, inbounds);
         }
     }
 }
+
+// Generate the inbound links report
+
+let inbounds = "url,status,redirected,location,inbounds";
 for (const item of data) {
-    if (item.status == 200) continue    // These are uninteresting
-    inbounds += `\n${item.location},${item.status},${map.get(item.location)?.join('|')}`;
+    // We are not interested in URLs
+    // that point to the right place
+    if (!item.redirected && item.status == 200) continue
+    // There could be lots of inbound links, so we output them line by line
+    for (const inbound of map.get(item.url)||[]) {
+        inbounds += `\n${item.url},${item.status},${item.redirected},${item.location},${inbound}`;
+    }
 }
 Deno.writeTextFileSync(`./data/${project}/inbounds.csv`, inbounds);
