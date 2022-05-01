ad366910d852d8e4192ec0963c6ef83365a78777
Jacob Conrad Martin <arctangent@gmail.com>
2022-05-01 01:18:08 +0100

    Updating tests to describe format with Deno 1.21

--- tests/database.test.ts
+++ tests/database.test.ts
@@ -1,40 +1,36 @@
 
-import { assertEquals } from "https://deno.land/std@0.115.1/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.136.0/testing/asserts.ts";
+import {describe, it} from "https://deno.land/std@0.136.0/testing/bdd.ts";
 
 import { Database } from '../database.ts';
 
-const db = new Database();
+describe('Database', () => {
 
-Deno.test({
-    name: 'database count does sums correctly',
-    fn: async () => {
+    const db = new Database();
+
+    it('count() does sums correctly', async () => {
         await db.flush();
         await db.enqueue('https://www.example.com/1');
         await db.enqueue('https://www.example.com/2');
         assertEquals(2, await db.count({}));
         await db.enqueue('https://www.example.com/3');
         assertEquals(3, await db.count({}));
-    }
-});
+    });
 
-Deno.test({
-    name: 'database ignores duplicate hrefs',
-    fn: async () => {
+    it('enqueue() ignores duplicate hrefs', async () => {
         await db.flush();
         const href = 'https://www.example.com';
         await db.enqueue(href);
         await db.enqueue(href);
         assertEquals(1, await db.count({}));
-    }
-});
+    });
 
-Deno.test({
-    name: 'database getQueued works correctly',
-    fn: async () => {
+    it('getQueued() works correctly', async () => {
         await db.flush();
         await db.enqueue('url1');
         await db.enqueue('url2');
         await db.update('url2', { status: 200 })
         assertEquals(['url1'], await db.getQueued());
-    }
+    });
+
 });
--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -1,14 +1,20 @@
 
-import { assertEquals } from "https://deno.land/std@0.115.1/testing/asserts.ts";
+import {
+    assertEquals
+} from "https://deno.land/std@0.136.0/testing/asserts.ts";
+import {
+    describe,
+    it,
+} from "https://deno.land/std@0.136.0/testing/bdd.ts";
 
 import { Parser } from '../parser.ts';
 
 const excludes = ['/ignore'];
 const parser = new Parser('https://example.com', excludes);
 
-Deno.test({
-    name: 'getURLs handles quoted and unquoted hrefs',
-    fn: () => {
+describe('getURLs', () => {
+
+    it('handles quoted and unquoted hrefs', () => {
         const input = `
             <a href='https://example.com/1'>1</a>
             <a href="https://example.com/2">2</a>
@@ -20,35 +26,26 @@ Deno.test({
             'https://example.com/3'
         ]
         assertEquals(expected, parser.getURLs(input));
-    }
-});
+    });
 
-Deno.test({
-    name: 'getURLs ignores self links',
-    fn: () => {
+    it('ignores self links', () => {
         const input = `
             <a href=https://example.com>naked</a>
             <a href=https://example.com/>trailing slash</a>
         `;
         const expected: string[] = [];
         assertEquals(expected, parser.getURLs(input));
-    }
-});
+    });
 
-Deno.test({
-    name: 'getURLs ignores zero length hrefs',
-    fn: () => {
+    it('ignores zero length hrefs', ()=> {
         const input = `
             <a href=>zero</a>
         `;
         const expected: string[] = [];
         assertEquals(expected, parser.getURLs(input));
-    }
-});
+    });
 
-Deno.test({
-    name: 'getURLs ignores anchor links',
-    fn: () => {
+    it('ignores anchor links',() => {
         const input = `
             <a href=#foo>naked anchor</a>
             <a href=https://example.com#foo>qualified anchor</a>
@@ -56,12 +53,9 @@ Deno.test({
         `;
         const expected: string[] = ['https://example.com/deep/link'];
         assertEquals(expected, parser.getURLs(input));
-    }
-});
+    });
 
-Deno.test({
-    name: 'getURLs ignores querystring',
-    fn: () => {
+    it('ignores querystring',() => {
         const input = `
             <a href=?foo>naked querystring</a>
             <a href=https://example.com?foo>qualified querystring</a>
@@ -69,46 +63,34 @@ Deno.test({
         `;
         const expected: string[] = ['https://example.com/deep/link'];
         assertEquals(expected, parser.getURLs(input));
-    }
-});
+    });
 
-Deno.test({
-    name: 'getURLs ignores non-https schemes',
-    fn: () => {
+    it('ignores non-https schemes',() => {
         const input = `
             <a href=tel:08005551234>phone</a>
             <a href=email:person@example.com>email</a>
         `;
         const expected: string[] = [];
         assertEquals(expected, parser.getURLs(input));
-    }
-});
+    });
 
-Deno.test({
-    name: 'getURLs ignores external links',
-    fn: () => {
+    it('ignores external links',() => {
         const input = `
             <a href=https://not-example.com>external site</a>
         `;
         const expected: string[] = [];
         assertEquals(expected, parser.getURLs(input));
-    }
-});
+    });
 
-Deno.test({
-    name: 'getURLs ignores href trailing space',
-    fn: () => {
+    it('ignores href trailing space',() => {
         const input = `
             <a href=https://example.com/foo attr=value>complicated anchor tag</a>
         `;
         const expected: string[] = ['https://example.com/foo'];
         assertEquals(expected, parser.getURLs(input));
-    }
-});
+    });
 
-Deno.test({
-    name: 'getURLs ignores excluded URLs',
-    fn: () => {
+    it('ignores excluded URLs',()=>{
         const input = `
             <a href=https://example.com/foo>included</a>
             <a href=https://example.com/ignore>excluded</a>
@@ -117,5 +99,6 @@ Deno.test({
         `;
         const expected: string[] = ['https://example.com/foo'];
         assertEquals(expected, parser.getURLs(input));
-    }
+    });
+
 });
